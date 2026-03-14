c7f9e4a0e6b7e23357f6f01d131f8c924b00ef80
venky7031 <venkyvenky7031@gmail.com>
2026-03-14 14:37:58 +0530

    hello world started

--- 01-variables.sh
+++ 01-variables.sh
@@ -3,8 +3,8 @@
 PERSON1=$1
 PERSON2=$2
 
-echo $1:: "how are you"
-echo $2:: "I am fine"
-echo $1:: "what are you doing"
-echo $2:: "nothing"
-echo $1:: "OK"
+echo $1(venky):: "how are you"
+echo $2(chota):: "I am fine"
+echo $1(venky):: "what are you doing"
+echo $2(chota):: "nothing"
+echo $1(venky):: "OK"
