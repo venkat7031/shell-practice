c1e1d41c103f86fafe4ac896410ece89365d9642
venky7031 <venkyvenky7031@gmail.com>
2026-03-14 15:34:48 +0530

    hello world started

---
+++ 02-special-vars.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+echo :: "all the variables passwd to the script: $@"
+echo :: "number of variables passed: $#"
+echo :: "script name: $0"
+echo :: "present working dir: $PWD"
+echo :: "home dir of the user: $HOME"
+echo :: "which user is running the script: $USER"
