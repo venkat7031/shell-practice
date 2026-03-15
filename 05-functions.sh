2d1c0ebbb9e62800fefe1a3e7b24bc0c55847bfb
venky7031 <venkyvenky7031@gmail.com>
2026-03-15 13:48:59 +0530

    functions

--- 05-functions.sh
+++ 05-functions.sh
@@ -3,18 +3,10 @@
 
 USERID=$(id -u)
 
-install packages () { 
+install_packages () { 
    dnf install nginx -y
    dnf install mysql -y
 }
-
-if [ $USERID -ne 0 ]
-then  
-    echo "ERROR: Please run this script with root access"
-    exit 1
-else
-    echo "your running with root access"
-fi
 
 if [ "$USERID" -eq 0 ]
 then
@@ -23,3 +15,4 @@ else
     echo "ERROR: please run with root access"
     exit 1
 fi
+install_packages
